9ce9a65d4b8ac48fad8ab23ccd4b9849c2376525
zjf <liu358813@gmail.com>
2026-04-18 14:52:11 +0800

    add run script

---
+++ run.sh
@@ -0,0 +1,16 @@
+python train.py \
+  --dataset data/coco_train2014 \
+  --style-image data/style_images/mosaic.jpg \
+  --preview-dir data/content_examples \
+  --output-dir checkpoints \
+  --run-name mosaic_custom \
+  --image-size 256 \
+  --style-size 512 \
+  --batch-size 8 \
+  --epochs 2 \
+  --subset-size 20000 \
+  --learning-rate 2e-4 \
+  --style-weight 1e5 \
+  --content-weight 1.0 \
+  --tv-weight 1e-6 \
+  --mixed-precision
